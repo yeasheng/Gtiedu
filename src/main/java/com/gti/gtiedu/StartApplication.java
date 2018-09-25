c48f1ba1979bff00615d6d5768a94cc89afe2cb2
yansheng <yansheng@126.com>
2018-09-25 21:02:27 +0800

    修改注册

--- src/main/java/com/gti/gtiedu/StartApplication.java
+++ src/main/java/com/gti/gtiedu/StartApplication.java
@@ -4,10 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@MapperScan("com.gti.gtiedu.dao")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@MapperScan("com.gti.gtiedu.dao")
 public class StartApplication {
 
 //	private static final Logger logger = LoggerFactory.getLogger(AbstractHttpJsonDecoder.class);
--- src/main/java/com/gti/gtiedu/controller/home/RegisterController.java
+++ src/main/java/com/gti/gtiedu/controller/home/RegisterController.java
@@ -1,13 +1,22 @@
 package com.gti.gtiedu.controller.home;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * 注册相关控制器
  *
  * @author yansheng
  * @create 2018-09-25 20:08
  **/
-
+@Controller
+@RequestMapping("/home")
 public class RegisterController {
 
-
+    @RequestMapping("/register")
+    public String registerPage(Model m){
+        m.addAttribute("name", "thymeleaf");
+        return "home/register";
+    }
 }
